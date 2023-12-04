bf1c2810c50c9e407c825d5f97eb78f07fc9ff6b
reagan <reaganm746@gmail.com>
2023-12-04 13:15:45 +0300

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+# lib.py
+
+import time
+
+def countdown(seconds):
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(seconds):
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        time.sleep(1)  # Pause for one second
+        seconds -= 1
+    print("HAPPY NEW YEAR!")
